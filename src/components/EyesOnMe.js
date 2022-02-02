06801bd229ae1ed3ec38c0fb518ae7ee9a898a58
Louise Iyengar <louise.iyengar@flatironschool.com>
2022-02-02 16:44:54 -0500

    commit tree 83e244cd8c7b43e9766ef0f9cf37376a4c6d221f

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,11 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+  //const text = "Eyes on me"
+  return (
+      <button onFocus={()=>console.log("Good!")} onBlur={()=>console.log("Hey! Eyes on me!")}>Eyes on me</button>
+  )
+}
+
+export default EyesOnMe
